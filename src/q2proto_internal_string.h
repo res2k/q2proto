fd24162a9bdd5814c1d23f1f854f682e6f6ced26
Frank Richter <frank.richter@gmail.com>
2024-11-01 15:50:04 +0100

    alloca fix for FreeBSD

--- src/q2proto_internal_string.h
+++ src/q2proto_internal_string.h
@@ -29,7 +29,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #if !defined(alloca)
 #define alloca _alloca
 #endif
-#else
+#elif !defined(__FreeBSD__)
 #include <alloca.h>
 #endif
 
